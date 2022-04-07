5b156d3c167c1f6cee4ceaa4349df623a100ff04
Justin Swift <jlswift2@gmail.com>
2022-04-06 21:04:34 -0500

    commit tree 857515904adac012881b878f3b219ff921f5ed80

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,34 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  rescue ActiveRecord::RecordNotFound => invalid
+    render json: { error: "Record not found"}, status: :not_found
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user, status: :ok
+  rescue ActiveRecord::RecordNotFound => invalid
+    render json: { error: "Record not found"}, status: :not_found
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    new_item = user.items.create(item_params)
+    render json: new_item, include: :user, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
   end
 
 end
